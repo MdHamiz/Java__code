36ba1acc2101def24b38ad86814ea0b460153199
Md Hamiz Imam <mdhamizimam@gmail.com>
2025-12-28 23:27:46 +0530

    string code

---
+++ src/String/check_Character.java
@@ -0,0 +1,13 @@
+package String;
+
+public class check_Character {
+    public static void main(String[] args) {
+        char c = 'g';
+        if (c =='a' || c =='e' || c=='i' ||c=='o'|| c== 'u'){
+            System.out.println( c + " is a vowel");
+        }
+        else{
+            System.out.println(c + " Is not a vowel");
+        }
+    }
+}
